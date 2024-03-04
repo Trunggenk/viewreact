68b5d5cbe34e796eba41d39c00bc59f0c9e05e1e
Trunggenk <22028170@vnu.edu.vn>
2024-03-04 15:03:51 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import{ ChatEngine}  from "react-chat-engine";
 import './App.css';
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="35a3e680-7f6a-4046-9a8f-cd06261830e2"
+            userName="admin"
+            userSecret="admin"
+        />
+    );
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
 }
-
 export default App;
